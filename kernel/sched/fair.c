d506491e4d82e64406313e3cb3a3d97440a343f3
celtare21 <celtare21@gmail.com>
2019-03-17 17:44:18 +0000

    sched: fair: Never debug max cpu_capacity
    This just spams the logs when we use deep suspend (with CPU hotplugging)
    and is not helpful.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8714,7 +8714,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	    (max_capacity < capacity)) {
 		mcc->val = capacity;
 		mcc->cpu = cpu;
-#ifdef CONFIG_SCHED_DEBUG
+#if 0
 		raw_spin_unlock_irqrestore(&mcc->lock, flags);
 		goto skip_unlock;
 #endif
