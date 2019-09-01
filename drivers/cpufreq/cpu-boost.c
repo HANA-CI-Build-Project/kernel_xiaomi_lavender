0e500b58439d3b2324dafe198b7c9bf1feda5d9e
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-09-01 11:18:22 +0700

    drivers: cpu-boost: fix this derp
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -200,7 +200,7 @@ static void do_input_boost_rem(struct work_struct *work)
 		i_sync_info = &per_cpu(sync_info, i);
 		i_sync_info->input_boost_min = 0;
 	}
-	
+
 	/* Update policies for all online CPUs */
 	update_policy_online();
 }
@@ -219,7 +219,6 @@ static void do_dynamic_stune_boost_rem(struct work_struct *work)
 	update_policy_online();
 
 }
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 void do_input_boost_max()
 {
