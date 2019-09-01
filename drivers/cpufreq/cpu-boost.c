951bff2709cabf55150611224ec0c2a91436d746
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-09-01 11:05:47 +0700

    drivers: cpu-boost: disable any input boost
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -33,10 +33,10 @@ struct cpu_sync {
 
 static DEFINE_PER_CPU(struct cpu_sync, sync_info);
 
-static unsigned int input_boost_enabled = 1;
+static unsigned int input_boost_enabled = 0;
 module_param(input_boost_enabled, uint, 0644);
 
-static unsigned int input_boost_ms = 40;
+static unsigned int input_boost_ms = 0;
 module_param(input_boost_ms, uint, 0644);
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
