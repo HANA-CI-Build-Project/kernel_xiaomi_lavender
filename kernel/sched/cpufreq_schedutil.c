e0243fb5d773e789495300251869ea0d1541db10
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-12-21 23:35:57 +0700

    sched: schedutil: tune a bit
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -749,7 +749,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
 	/* Hard-code some sane rate-limit values */
-	tunables->up_rate_limit_us = 1000;
+	tunables->up_rate_limit_us = 500;
 	tunables->down_rate_limit_us = 20000;
 
 	policy->governor_data = sg_policy;
